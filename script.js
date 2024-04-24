dc16886dafa82ab456aeea01d2d19d7f302c35ef
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-04-24 08:36:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    audio.play();
+  });
+});
